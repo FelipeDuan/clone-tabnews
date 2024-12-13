9336776c567169cbff0d76cef816d7b1a69f6359
Felipe Duan <felipeduan1@gmail.com>
2024-12-13 19:02:18 -0300

    adc pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1> Curiosidade: A Terra, que é redonda, não custa nada lembrar, gira no seu próprio eixo no sentido anti-horário, partindo do ponto de vista do polo Norte. Isso significa que ela gira no sentido oeste-leste, por isso o movimento aparente do sol, para nós, é do leste para o oeste. O movimento completo, como você deve imaginar, dura quase 24 horas: mais precisamente, 23 horas 56 minutos 4 segundos e 9 centésimos.</h1>
+}
+
+export default Home; 
